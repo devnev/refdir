1f040383c5f7ebfa8947412cd10359cdfb9864a1
Mark Nevill <mark.nevill@gmail.com>
2025-02-05 10:04:15 +0800

    Add golangci-lint module plugin registration

--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.22.0
 
 toolchain go1.23.2
 
-require golang.org/x/tools v0.29.0
+require (
+	github.com/golangci/plugin-module-register v0.1.1
+	golang.org/x/tools v0.29.0
+)
 
 require (
 	golang.org/x/mod v0.23.0 // indirect
 	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
 )
--- golangci-lint/plugins.go
+++ golangci-lint/plugins.go
@@ -1,5 +1,25 @@
-package main
+package golangcilint
 
 import (
-	_ "github.com/devnev/refdir/golangci-lint"
+	"github.com/devnev/refdir/analysis/refdir"
+	"github.com/golangci/plugin-module-register/register"
+	"golang.org/x/tools/go/analysis"
 )
+
+func init() {
+	register.Plugin("refdir", New)
+}
+
+func New(settings any) (register.LinterPlugin, error) {
+	return &Plugin{}, nil
+}
+
+type Plugin struct{}
+
+func (p *Plugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
+	return []*analysis.Analyzer{refdir.Analyzer}, nil
+}
+
+func (p *Plugin) GetLoadMode() string {
+	return register.LoadModeTypesInfo
+}
