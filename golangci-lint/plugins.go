262ccaf22965fb369d76d6516d2a80b39c9480b3
Mark Nevill <mark.nevill@gmail.com>
2025-02-05 19:36:47 +0800

    Parse settings for golangci-lint plugin

--- golangci-lint/plugins.go
+++ golangci-lint/plugins.go
@@ -1,6 +1,9 @@
 package golangcilint
 
 import (
+	"fmt"
+	"slices"
+
 	"github.com/devnev/refdir/analysis/refdir"
 	"github.com/golangci/plugin-module-register/register"
 	"golang.org/x/tools/go/analysis"
@@ -10,13 +13,34 @@ func init() {
 	register.Plugin("refdir", New)
 }
 
-func New(settings any) (register.LinterPlugin, error) {
-	return &Plugin{}, nil
+func New(rawSettings any) (register.LinterPlugin, error) {
+	settings, err := register.DecodeSettings[map[string]string](rawSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	parsedOrder := map[refdir.RefKind]refdir.Direction{}
+	for key, value := range settings {
+		if !slices.Contains(refdir.RefKinds, refdir.RefKind(key)) {
+			return nil, fmt.Errorf("invalid refdir settings key %q", key)
+		}
+		if !slices.Contains(refdir.Directions, refdir.Direction(value)) {
+			return nil, fmt.Errorf("invalid refdir direction %q for settings key %q", value, key)
+		}
+		parsedOrder[refdir.RefKind(key)] = refdir.Direction(value)
+	}
+
+	return &Plugin{settings: parsedOrder}, nil
 }
 
-type Plugin struct{}
+type Plugin struct {
+	settings map[refdir.RefKind]refdir.Direction
+}
 
 func (p *Plugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
+	for kind, dir := range p.settings {
+		refdir.RefOrder[kind] = dir
+	}
 	return []*analysis.Analyzer{refdir.Analyzer}, nil
 }
 
